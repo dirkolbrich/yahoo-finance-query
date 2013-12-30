d82a08cd2f94f1c888bbeac5f422a52630863b38
Dirk Olbrich <mail@dirkolbrich.de>
2013-12-30 09:19:16 +0100

    Create YahooFinanceQuery.php

---
+++ YahooFinanceQuery.php
@@ -0,0 +1,15 @@
+<?php
+namespace YahooFinanceQuery
+
+class YahooFinanceQuery
+{
+    public config = array();
+
+    public function __construct()
+    {
+    }
+
+    public function config()
+    {
+    }
+}
