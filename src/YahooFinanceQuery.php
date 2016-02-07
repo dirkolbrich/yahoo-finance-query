2ddfdeb9309b2ee53b7e5cdc66f5b374ecde3c0a
Dirk Olbrich <mail@dirkolbrich.de>
2016-02-07 15:50:53 +0100

    clean up main file

--- src/YahooFinanceQuery.php
+++ src/YahooFinanceQuery.php
@@ -28,17 +28,31 @@ class YahooFinanceQuery
         'returnType' => 'array', // 'array' or 'json'
         );
 
+    /**
+     * @var string
+     */
     protected $query;
+
+    /**
+     * @var bool
+     */
     protected $raw = false;
+
+    /**
+     * @var bool
+     */
     protected $yql = false;
+
+    /**
+     * @var bool
+     */
     protected $toJson = false;
 
     /**
      * constructor with optional config param
-     * @param array $config - key => value for configuration
-     * @return void
+     * @param array $config - key / value for configuration
      */
-    public function __construct(array $config = array())
+    public function __construct($config = array())
     {
         if (!empty($config)) {
             $this->config($config);
@@ -47,22 +61,20 @@ class YahooFinanceQuery
 
     /**
      * creator with optional config param
-     *
-     * @param array $config - key => value for configuration
+     * @param array $config - key / value for configuration
      * @return YahooFinanceQuery $query
      */
-    public static function make(array $config = array())
+    public static function make($config = array())
     {
         return new YahooFinanceQuery($config);
     }
 
     /**
      * configurator
-     *
      * @param array $config - key => value for configuration
      * @return self
      */
-    public function config(array $config = array())
+    public function config($config = array())
     {
         foreach ($config as $key => $setting) {
             if (array_key_exists($key, $this->config)) {
@@ -74,8 +86,7 @@ class YahooFinanceQuery
 
     /**
      * get configurator setting
-     *
-     * @return array $this-config
+     * @return array $config
      */
     public function getConfig()
     {
@@ -83,8 +94,7 @@ class YahooFinanceQuery
     }
 
     /**
-     * as it says, returns the raw response from a request
-     *
+     * return the raw response from a request
      * @return self
      */
     public function raw()
@@ -95,7 +105,6 @@ class YahooFinanceQuery
 
     /**
      * directs all requests via the YQL console, default if possible is requests via .csv
-     *
      * @return self
      */
     public function yql()
@@ -105,9 +114,8 @@ class YahooFinanceQuery
     }
 
     /**
-     * as it says, get the results
-     *
-     * @return $data
+     * get results from the query
+     * @return string $data
      */
     public function get()
     {
@@ -127,7 +135,6 @@ class YahooFinanceQuery
 
     /**
      * return the result as JSON format
-     *
      * @return self
      */
     public function toJson()
@@ -139,7 +146,6 @@ class YahooFinanceQuery
     /**
      * get a list of stocks with symbol and market for provided search string
      * from yahoo.finance.com's stock symbol autosuggest callback
-     *
      * @param string $searchString - the name/string to search for
      * @return self
      */
@@ -147,38 +153,34 @@ class YahooFinanceQuery
     {
         $query = new SymbolSuggest($this->yql);
         $this->query = $query->query($searchString);
-
         return $this;
     }
 
     /**
      * get stock quotes for provided symbols from yahoo.finance.com
-     *
      * @param array $symbolList - array with symbol/s
      * @param array $searchParams - array with query params
      * @return self
      */
-    public function quote(array $symbolList, array $searchParams = null)
+    public function quote($symbolList, $searchParams = null)
     {
         $query = new CurrentQuote($this->yql);
         $this->query = $query->query($symbolList, $searchParams);
-
         return $this;
     }
 
     /**
      * get historical quotes for provided symbol from yahoo.finance.com, direct query to csv
-     *
      * @param string $symbol
      * @param string $startDate - yyyy-mm-dd
      * @param string $endDate - yyyy-mm-dd
      * @param string $param - type of data
+     * @return self
      */
     function historicalQuote($symbol, $startDate = '', $endDate = '', $param = 'd')
     {
         $query = new HistoricalQuote($this->yql);
         $this->query = $query->query($symbol, $startDate, $endDate, $param);
-
         return $this;
     }
 
@@ -186,55 +188,49 @@ class YahooFinanceQuery
      * get intraday quotes for provided symbol from yahoo.finance.com, direct query to csv
      *
      * @param string $symbol
+     * @param string $period
      * @param string $param - type of data
-     * @return array $quoteList - array with quotes
      * @return self
      */
     function intraDay($symbol, $period = '1d', $param = 'quote')
     {
         $query = new IntraDayQuote($this->yql);
         $this->query = $query->query($symbol, $period, $param);
-
         return $this;        
     }
 
     /**
      * get list of info for provided symbol from yahoo.finance.com
-     *
      * @param string $symbol - stock symbol
+     * @return self
      */
     function stockInfo($symbol)
     {
         $query = new StockInfo($this->yql);
         $this->query = $query->query($symbol);
-
         return $this;
     }
 
     /**
      * get list of component for indices symbol from yahoo.finance.com
-     *
-     * @param array $symbol
+     * @param array $symbols
+     * @return self
      */
-    function indexList(array $symbols)
+    function indexList($symbols)
     {
         $query = new IndexList($this->yql);
         $this->query = $query->query($symbols);
-
         return $this;
     }
 
     /**
-    *   get full list of sectors with corresponding industries from yahoo.finance.com
-    *
-    *   @return self
+     * get full list of sectors with corresponding industries from yahoo.finance.com
+     * @return self
     */
     function sectorList()
     {
         $query = new SectorList($this->yql);
         $this->query = $query->query();
-
         return $this;
     }
-
 }
